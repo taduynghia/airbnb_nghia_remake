9604b34e310c07f41dda69c572b61c44030d6931
unknown <nghiadkc09@gmail.com>
2022-01-06 16:27:43 +0700

    “git_add_new_airbnbproject

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>© 2022 Airbnb Clone - demo  ♡ make by <b>Ta Duy Nghia  </b> from Youtube channel : <b>Clever Programmer</b> </p>
+            <p>Privacy · Terms · Sitemap · Company Details </p>
+        </div>
+    )
+}
+
+export default Footer
